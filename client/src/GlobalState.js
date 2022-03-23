7e32ae02408be23cacec4c715637fb076e23fb7e
omar hallomi <omarhallomi7@gmail.com>
2022-03-23 13:49:09 +0200

    new

---
+++ client/src/GlobalState.js
@@ -0,0 +1,35 @@
+import React,{createContext, useState,useEffect} from "react";
+import ProductsAPI from "./api/ProductsAPI";
+import UserAPI from "./api/userAPI"
+import axios from "axios";
+
+export const GlobalState = createContext()
+
+export const DataProvider = ({children}) =>{
+    const [token, setToken] = useState(false)
+    
+    const refreshToken = async () =>{
+        const res = await axios.get('user/refresh_token')
+        
+        setToken(res.data.accesstoken)
+    }
+    
+    useEffect(() => {
+        const firstLogin = localStorage.getItem('firstLogin')
+        if(firstLogin)
+            refreshToken()
+    },[])
+
+    const state = {
+        token: [token, setToken],
+        productsAPI: ProductsAPI(),
+        userAPI: UserAPI(token)
+    }
+    
+    return (
+        <GlobalState.Provider value={state}>
+            {children}
+        </GlobalState.Provider>
+    )
+}
+
---
+++ routes/categoryRouter.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+const categoryCtrl = require('../controller/categoryCtrl')
+const authAdmin = require('../middleware/authAdmin')
+const auth = require('../middleware/auth')
+
+router.route('/category')
+    .get(categoryCtrl.getCategories)
+    .post(auth,authAdmin,categoryCtrl.createCategory)
+
+router.route('/category/:id')
+    .delete(auth,authAdmin,categoryCtrl.deleteCategory)
+    .put(auth,authAdmin,categoryCtrl.updateCategory)
+
+
+
+module.exports = router
