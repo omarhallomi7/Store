d21f7279fe4f119763baa844bc2ed301e9492ec8
omarhallomi <omarhallomi7@gmail.com>
2022-03-24 10:32:05 +0200

    ..

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3001 ;
+const port = process.env.PORT || 3001 ;
 const mongoose = require('mongoose');
 const http = require('http');
 const cors = require('cors');
