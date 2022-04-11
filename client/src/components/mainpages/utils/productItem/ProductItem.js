b293cfe48cc104e73b96a44f19a9eaa072f035c1
omarhallomi <omarhallomi7@gmail.com>
2022-04-11 16:40:41 +0300

    price to NIS

--- client/src/components/mainpages/utils/productItem/ProductItem.js
+++ client/src/components/mainpages/utils/productItem/ProductItem.js
@@ -14,7 +14,7 @@ function ProductItem({ product, isAdmin, deleteProduct, handleCheck }) {
       <img src={product.images.url} alt="" />
       <div className="product_box">
         <h2 title={product.title}> {product.title} </h2>
-        <span>${product.price}</span>
+        <span>{product.price}</span>
         <p>{product.description}</p>
       </div>
 
